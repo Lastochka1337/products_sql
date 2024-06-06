29243dc979f0e13e2a3eb04d6b9477656308e67f
Lastochka1337 <shurigin.igor12@gmail.com>
2024-06-06 22:21:19 +0300

    DB and code added

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import sqlite3
 
+db = sqlite3.connect("shop.db")
+
+db.execute('''CREATE TABLE products (
+    product_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    category TEXT NOT NULL,
+    price REAL NOT NULL
+);
+''')
+
+db.execute('''CREATE TABLE customers ( customer_id INTEGER PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL, email TEXT NOT NULL UNIQUE );
+''')
+
+db.execute('''CREATE TABLE orders ( order_id INTEGER PRIMARY KEY, customer_id INTEGER NOT NULL, product_id INTEGER NOT NULL, quantity INTEGER NOT NULL, 
+           order_date DATE NOT NULL, FOREIGN KEY (customer_id) REFERENCES customers(customer_id), FOREIGN KEY (product_id) REFERENCES products(product_id) );
+''')
